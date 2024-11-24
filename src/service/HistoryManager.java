4c7bac5c639e3b2125f0ef3d2b05f60a1c3a716f
OlgaPegushina <pegushina.shine@yandex.ru>
2024-11-24 20:42:55 +0300

    Спринт 6. Изменения хранения истории просмотра задач на LinkedList и удаление дублей в истории

--- src/service/HistoryManager.java
+++ src/service/HistoryManager.java
@@ -6,5 +6,6 @@ import java.util.List;
 
 public interface HistoryManager {
     void addInHistory(Task task);
+    void removeFromHistory(int id);
     List<Task> getHistory();
 }
--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -3,28 +3,72 @@ package service;
 import model.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private static final int SIZE_HISTORY = 10;
-    private final List<Task> history = new ArrayList<>(SIZE_HISTORY);
-
-    private void removeHistoryFirst() {
-        if (!history.isEmpty() && history.size() == SIZE_HISTORY) {
-            history.removeFirst();
-        }
-    }
+    private Node head = null;
+    private Node tail = null;
+    private final Map<Integer, Node> listHistory = new HashMap<>();
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(history);
+        return getTasks();
     }
 
     @Override
     public void addInHistory(Task task) {
         if (task != null) {
-            removeHistoryFirst();
-            history.add(task);
+            linkLast(task);
+        }
+    }
+
+    private void linkLast(Task task) {
+        Node newNode = new Node (task, null, this.tail);
+        if (this.tail == null) {
+            this.head = newNode;
+        } else {
+            tail.setNext(newNode);
+        }
+        this.tail = newNode;
+        removeFromHistory(task.getId());
+        listHistory.put(task.getId(), newNode);
+    }
+
+    @Override
+    public void removeFromHistory(int id) {
+        if (listHistory.containsKey(id)) {
+            removeNode(listHistory.get(id));
+        }
+    }
+
+    private List<Task> getTasks() {
+        List<Task> listHistoryRes = new ArrayList<>();
+        Node node = head;
+        while (node != null) {
+            listHistoryRes.add(node.getItem());
+            node = node.getNext();
+        }
+        return listHistoryRes;
+    }
+
+    private void removeNode(Node node) {
+        if (node != null) {
+            Node nodeNext = node.getNext();
+            Node nodePrev = node.getPrev();
+            if (nodePrev == null) {
+                head = nodeNext;
+                head.setPrev(null);
+            } else {
+                nodePrev.setNext(nodeNext);
+            }
+            if (nodeNext == null) {
+                tail = nodePrev;
+                tail.setNext(null);
+            } else {
+                nodeNext.setPrev(nodePrev);
+            }
         }
     }
 }
